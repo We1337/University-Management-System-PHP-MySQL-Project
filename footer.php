b203ca974b040e6e44f9a2b788625126f860614f
We1337 <githubdevice@gmail.com>
2023-10-20 07:48:54 +0600

    feater: jquery script removed

--- footer.php
+++ footer.php
@@ -1,37 +1,5 @@
 
 
-        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.12.0.min.js"><\/script>')</script>
-        <script src="js/plugins.js"></script>
-        <script src="js/main.js"></script>
-		<script type="text/javascript">
-			$(document).ready(function() {
-				var s = $("#sticker");
-				var pos = s.position();                    
-				$(window).scroll(function() {
-					var windowpos = $(window).scrollTop();
-					if (windowpos >= pos.top) {
-						s.addClass("stick");
-					} else {
-						s.removeClass("stick"); 
-					}
-				});
-			});
-		</script>
-		<script type="text/javascript">
-			$(document).ready(function() {
-				var s = $("#stickerside");
-				var pos = s.position();                    
-				$(window).scroll(function() {
-					var windowpos = $(window).scrollTop();
-					if (windowpos >= pos.top) {
-						s.addClass("stickside");
-					} else {
-						s.removeClass("stickside"); 
-					}
-				});
-			});
-		</script>
-
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.8.1/flowbite.min.js"></script>
 
     </body>
