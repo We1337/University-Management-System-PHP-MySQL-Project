e2a78afd1ba7f449f913cb392b8d4d462a1177c1
We1337 <weuser149@gmail.com>
2023-10-24 14:44:08 +0600

    refactor: code update, commented, documented.

--- admin/includes/admin.class.php
+++ admin/includes/admin.class.php
@@ -10,29 +10,32 @@ class Admin extends DatabaseConnection {
 	 *
 	 * @return PDOStatement|false Returns a PDOStatement object on success or false on failure.
 	 */
-	public function get_all_student()
-	{
+	public function fetchAllStudentInformation() {
 		// Define the SQL query to retrieve all student information
-		$query_value = "SELECT * FROM st_info ORDER BY st_id ASC";
-		
-		try
-		{
-			// Prepare the SQL query and execute it
-			$query = parent::connect()->prepare($query_value);
-			$query->execute();
-			
-			// Return the PDOStatement object
-			return $query;
-		}
-		catch(PDOException $e)
-		{
+		$sqlQuery = "SELECT * FROM `st_info` ORDER BY `st_id` ASC";
+
+		try {
+			// Prepare the SQL query
+			$query = parent::connect()->prepare($sqlQuery);
+
+			if ($query) {
+				// Execute the query
+				$result = $query->execute();
+
+				if ($result) {
+					// Return the PDOStatement object
+					return $query;
+				} 
+			} 
+		} catch(PDOException $e) {
 			// Log any database errors that occur
 			error_log("Database Error: " . $e->getMessage());
+			// You may also consider re-throwing the exception here for further error handling.
 		}
-		
-		// Return null if an error occurs
-		return null;
+
+		return false; // Database error
 	}
+
 
 
 	/**
@@ -41,39 +44,39 @@ class Admin extends DatabaseConnection {
 	 * This function performs a search for students in the "st_info" table based on the provided query.
 	 * It searches for matches in the student ID, name, contact, and email fields and returns the results.
 	 *
-	 * @param string $query The search query.
+	 * @param string $searchQuery The search query.
 	 * @return PDOStatement|false Returns a PDOStatement object with the search results on success or false on failure.
 	 */
-	public function search($user_input)
-	{
+	public function searchStudentsByQuery($searchQuery) {
 		// Define the SQL query for searching students
-		$query_value = "SELECT * FROM st_info WHERE (st_id LIKE :query OR name LIKE :query OR contact LIKE :query OR email LIKE :query) ORDER BY st_id";
+		$sqlQuery = "SELECT * FROM `st_info` WHERE (`st_id` LIKE :query OR `name` LIKE :query OR `contact` LIKE :query OR `email` LIKE :query) ORDER BY `st_id`";
 		
-		try
-		{
+		try {
 			// Prepare the SQL query
-			$pdo = parent::connect();
-			$query = $pdo->prepare($query_value);
+			$query = parent::connect()->prepare($sqlQuery);
 			
-			// Bind the search query as a parameter (using prepared statements to prevent SQL injection)
-			$query->bindValue(':query', '%' . $user_input . '%');
-			
-			// Execute the query
-			$query->execute();
-			
-			// Return the PDOStatement object with search results
-			return $query;
-		}
-		catch(PDOException $e)
-		{
+			if($query) {
+				// Bind the search query as a parameter (using prepared statements to prevent SQL injection)
+				$query->bindValue(':query', '%' . $searchQuery . '%');
+				
+				// Execute the query
+				$result = $query->execute();
+				
+				if ($result) {
+					// Return the PDOStatement object with search results
+					return $query;
+				} 
+			}
+		} catch(PDOException $e) {
 			// Log any database errors that occur
 			error_log("Database Error: " . $e->getMessage());
+			// You may also consider re-throwing the exception here for further error handling.
 		}
-		
-		// Return null if an error occurs
-		return null;
+
+		return false; // Database error
 	}
-	
+
+
 
 	/**
 	 * Admin Login Function
@@ -81,52 +84,53 @@ class Admin extends DatabaseConnection {
 	 * This function is responsible for authenticating an admin user by checking their
 	 * credentials in the database.
 	 *
-	 * @param string $username The admin's username.
-	 * @param string $password The admin's password.
+	 * @param string $adminUsername The admin's username.
+	 * @param string $adminPassword The admin's password.
 	 *
 	 * @return bool Returns true if the login is successful, false otherwise.
 	 */
-	public function admin_login($username, $password)
-	{
+	public function authenticateAdmin($adminUsername, $adminPassword) {
 		// Define the SQL query with placeholders for username and password
-		$query_value = "SELECT `id`, `username` FROM `admin` WHERE `username` = :username AND `password` = :password";
+		$sqlQuery = "SELECT `id`, `username` FROM `admin` WHERE `username` = :username AND `password` = :pass_word";
 
-		try 
-		{
+		try {
 			// Prepare the SQL statement
-			$stmt = parent::connect()->prepare($query_value);
+			$query = parent::connect()->prepare($sqlQuery);
 
-			// Bind the parameters to the placeholders
-			$stmt->bindParam(':username', $username, PDO::PARAM_STR);
-			$stmt->bindParam(':password', $password, PDO::PARAM_STR);
+			if ($query) {
+				// Bind the parameters to the placeholders
+				$query->bindParam(':username', $adminUsername, PDO::PARAM_STR);
+				$query->bindParam(':pass_word', $adminPassword, PDO::PARAM_STR);
 
-			// Execute the query
-			$stmt->execute();
+				// Execute the query
+				$query->execute();
 
-			// Get the count of rows that match the query
-			$count = $stmt->rowCount();
+				// Get the count of rows that match the query
+				$rowCount = $query->rowCount();
 
-			if ($count == 1)
-			{
-				// Fetch the admin information
-				$admin_info = $stmt->fetch(PDO::FETCH_ASSOC);
+				if ($rowCount == 1) {
+					// Fetch the admin information
+					$adminInformation = $query->fetch(PDO::FETCH_ASSOC);
 
-				// Set session variables for successful login
-				$_SESSION['admin_login'] = true;
-				$_SESSION['admin_id'] = $admin_info['id'];
-				$_SESSION['admin_name'] = $admin_info['username'];
-
-				return true; // Successful login
+					if ($adminInformation) {
+						// Set session variables for successful login
+						$_SESSION['admin_login'] = true;
+						$_SESSION['admin_id'] = $adminInformation['id'];
+						$_SESSION['admin_name'] = $adminInfomation['username'];
+						
+						return true; // Successful login
+					}
+				}
 			}
-		} 
-		catch(PDOException $e) 
-		{
-			// Handle database errors gracefully, log the error, and return false
+		} catch(PDOException $e) {
+			// Handle database errors gracefully, log the error
 			error_log("Database Error: " . $e->getMessage());
+			// You may also consider re-throwing the exception here for further error handling.
 		}
 
 		return false; // Login failed
 	}
+
 
 
 	/**
@@ -141,6 +145,7 @@ class Admin extends DatabaseConnection {
 		// Use the isset() function to check if the 'admin_login' key is set in the session.
 		return isset($_SESSION['admin_login']);
 	}
+
 
 
 	/**
@@ -159,43 +164,40 @@ class Admin extends DatabaseConnection {
 	}
 
 
+
 	/**
 	 * Delete a student by their Student ID.
 	 *
 	 * @param string $student_id The unique Student ID to identify the student to be deleted.
 	 * @return bool Returns true on successful deletion, or false on failure.
 	 */
-	public function delete_student($student_id)
-	{
+	public function removeStudentById($studentId) {
 		// Define the SQL query to delete a student by their Student ID
-		$query_value = "DELETE FROM st_info WHERE st_id = :student_id";
+		$sqlQuery = "DELETE FROM `st_info` WHERE `st_id` = :student_id";
 
-		try
-		{
+		try {
 			// Get a PDO instance for the database connection
-			$pdo = parent::connect();
+			$query = parent::connect()->prepare($sqlQuery);
 
-			// Prepare the SQL query
-			$stmt = $pdo->prepare($query_value);
+			if ($query) {
+				// Bind the Student ID as a parameter to prevent SQL injection
+				$query->bindParam(":student_id", $student_id, PDO::PARAM_STR);
 
-			// Bind the Student ID as a parameter to prevent SQL injection
-			$stmt->bindParam(":student_id", $student_id, PDO::PARAM_STR);
+				// Execute the query and check the result
+				$result = $query->execute();
 
-			// Execute the query and check the result
-			$result = $stmt->execute();
-
-			if ($result) {
-				return true; // Deletion was successful
+				if ($result) {
+					return true; // Deletion was successful
+				}	
 			}
-		} 
-		catch (PDOException $e)
-		{
+		} catch (PDOException $e) {
 			// Log any database errors that occur
 			error_log("Database Error: " . $e->getMessage());
 		}
 
 		return false; // Deletion failed
 	}
+
 
 
 	/**
@@ -205,76 +207,167 @@ class Admin extends DatabaseConnection {
 	 *
 	 * @return PDOStatement|false|null A PDOStatement with the result set, false on query failure, or null on exception.
 	 */
-	public function fetchStudentAttendanceRecords()
-	{
-		$query = "SELECT * FROM at_student";
+	public function fetchStudentAttendanceRecords() {
+		$sqlQuery = "SELECT * FROM `at_student`";
 		
 		try {
 			// Get a database connection using the parent's 'connect' method.
-			$pdo = parent::connect();
+			$query = parent::connect()->prepare($sqlQuery);
 
-			// Prepare the SQL query.
-			$stmt = $pdo->prepare($query);
+			if ($query) {
+				// Execute the query.
+				$result = $query->execute();
 
-			// Execute the query.
-			$result = $stmt->execute();
-
-			if ($result) {
-				return $stmt; // Return the PDOStatement with the result set.
+				if ($result) {
+					// Return the PDOStatement with the result set.
+					return $query;
+				}
 			}
 		} catch (PDOException $e) {
+			// Log any database errors that occur.
+			error_log("Database Error: " . $e->getMessage());
+			// You may also consider re-throwing the exception here for further error handling.
+		}
+		
+		// Return false on query failure or null on exception.
+		return false;
+	}
+
+
+
+	/**
+	 * Insert student attendance records into the database.
+	 *
+	 * This function inserts records into the 'at_student' and 'attn' tables to track student attendance.
+	 *
+	 * @param string $student_name The name of the student.
+	 * @param int $student_id The student's ID.
+	 * @return bool Returns true on successful insertion, false on failure.
+	 */
+	public function insertStudentAttendance($student_name, $student_id) {
+		$sqlQuery_at_student = "INSERT INTO `at_student` (student_name, student_id) VALUES (:student_name, :student_id)";
+		$sqlQuery_at_attendance = "INSERT INTO `attn` (st_id) VALUES (:student_id)";
+
+		try {
+			$query_at_student = parent::connect()->prepare($sqlQuery_at_student);
+			$query_at_attendance = parent::connect()->prepare($sqlQuery_at_attendance);
+
+			if ($query_at_student && $query_at_attendance) {
+				// Bind parameters to the prepared statements.
+				$query_at_student->bindParam(':student_name', $student_name, PDO::PARAM_STR);
+				$query_at_student->bindParam(':student_id', $student_id, PDO::PARAM_INT);
+
+				$query_at_attendance->bindParam(':student_id', $student_id, PDO::PARAM_INT);
+
+				// Execute both queries.
+				$result_student = $query_at_student->execute();
+				$result_attendance = $query_at_attendance->execute();
+
+				if ($result_student && $result_attendance) {
+					return true; // Successful insertion.
+				}
+			}
+		} catch(PDOException $e) {
+			// Log any database errors that occur.
 			error_log("Database Error: " . $e->getMessage());
 		}
 		
-		return null;
+		return false; // Insertion failed.
 	}
 
-
-
-	public function add_attn_student($name,$stid){
-		global $conn;
-		$sql = "insert into at_student(name,st_id) values('$name','$stid')";
-		$result = $conn->query($sql);
-		
-		$sql2 = "insert into attn(st_id) values('$stid')";
-		$result = $conn->query($sql2);
-		return $result;
-	}
 	
 
-	public function insertattn($cur_date,$atten = array()){
-		global $conn;
-		$sql = "select distinct at_date from attn";
-		$result = $conn->query($sql);
-		while($row = $result->fetch_assoc()){
-			$db_date = $row['at_date'];
-			if($cur_date == $db_date){
-				return false;
+
+	/**
+	 * Insert student attendance records for a specific date into the database.
+	 *
+	 * This function inserts attendance records into the 'attn' table for a given date, while ensuring there are no duplicates.
+	 *
+	 * @param string $current_date The date for which attendance records should be inserted.
+	 * @param array $attendance An array containing student attendance data (student ID as keys and 'present' or 'absent' as values).
+	 * @return bool Returns true on successful insertion, false if the date already exists or an error occurs.
+	 */
+	public function insertStudentAttendanceForDate($current_date, $attendance = array()) {
+		// Check if the date already exists in the database to prevent duplicates.
+		$sqlCheckDate = "SELECT COUNT(*) as `count` FROM `attn` WHERE `at_date` = :current_date";
+
+		try {
+			$stmtCheckDate = parent::connect()->prepare($sqlCheckDate);
+
+			if ($stmtCheckDate) {
+				$stmtCheckDate->bindParam(':current_date', $current_date, PDO::PARAM_STR);
+				$stmtCheckDate->execute();
+				$dateCount = $stmtCheckDate->fetch(PDO::FETCH_ASSOC)['count'];
+
+				if ($dateCount > 0) {
+					return false; // Date already exists, no insertion.
+				}
+
+				// Insert attendance records based on the provided data.
+				foreach ($attendance as $key => $attn_value) {
+					$sqlInsertAttendance = "INSERT INTO `attn` (`st_id`, `atten`, `at_date`) VALUES (:student_id, :attendance, :current_date)";
+
+					$stmtInsertAttendance = parent::connect()->prepare($sqlInsertAttendance);
+					if ($stmtInsertAttendance) {
+						$stmtInsertAttendance->bindParam(':student_id', $key, PDO::PARAM_INT);
+						$stmtInsertAttendance->bindParam(':attendance', $attn_value, PDO::PARAM_STR);
+						$stmtInsertAttendance->bindParam(':current_date', $current_date, PDO::PARAM_STR);
+
+						if (!$stmtInsertAttendance->execute()) {
+							return false; // Insertion failed.
+						}
+					}
+				}
+				return true; // Successful insertion.
 			}
+		} catch(PDOException $e) {
+			// Log any database errors that occur
+			error_log("Database Error: " . $e->getMessage());
+			// You may also consider re-throwing the exception here for further error handling.
 		}
-		foreach($atten as $key =>$attn_value ){
-			if($attn_value == "present"){
-				$sql = "insert into attn(st_id,atten,at_date) values('$key','present','$cur_date')";
-				$att_res = $conn->query($sql);
-			}elseif($attn_value == "absent"){
-				$sql = "insert into attn(st_id,atten,at_date) values('$key','absent','$cur_date')";
-				$att_res = $conn->query($sql);
-			}
-		}
-		if($att_res){
-			return true;
-		}else{
-			return false;
-		}
-		
+		return false; // Insertion failed.
 	}
 
 
-	public function delete_atn_student($at_id){
-		global $conn;
-		$res = $conn->query("delete from at_student where id = '$at_id' ");
-		return $res;
+
+	/**
+	 * Delete a student record from the 'at_student' table.
+	 *
+	 * This function deletes a student record identified by their unique student ID from the 'at_student' table.
+	 *
+	 * @param int $studentId The unique identifier of the student to be deleted.
+	 * @return bool Returns true if the student record is successfully deleted, false if an error occurs.
+	 */
+	public function deleteAtnStudent($studentId) {
+		// Prepare the SQL query to delete a student record by ID.
+		$sqlQuery = "DELETE FROM at_student WHERE id = :student_id";
+
+		try {
+			// Get a database connection and prepare the SQL query.
+			$query = parent::connect()->prepare($sqlQuery);
+
+			// Bind the student ID parameter to the query.
+			$query->bindParam(':student_id', $studentId, PDO::PARAM_INT);
+
+			// Execute the query to delete the student record.
+			$result = $query->execute();
+
+			// Check if the query was executed successfully.
+			if ($result) {
+				// Return true to indicate a successful deletion.
+				return true;
+			}
+
+		} catch (PDOException $e) {
+			// Log any database errors to the error log.
+			error_log("Database Error: " . $e->getMessage());
+			// You may also consider re-throwing the exception here for further error handling.
+		}
+
+		// Return false in case of an error or unsuccessful deletion.
+		return false;
 	}
+
 
 
 	public function get_attn_date(){
