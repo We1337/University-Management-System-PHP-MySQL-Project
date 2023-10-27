860da6f2851ee13ffdcb24d6a758226b5e12e756
We1337 <weuser149@gmail.com>
2023-10-27 22:31:07 +0600

    refactor: layout of the page updated

--- add_result.php
+++ add_result.php
@@ -1,73 +1,83 @@
 <?php
-session_start();
-	require "php/config.php";
-	require_once "php/functions.php";
-	$user = new login_registration_class();
-	$admin_id = $_SESSION['admin_id'];
-	$admin_name = $_SESSION['admin_name'];
-	if(!$user->get_admin_session()){
+	session_start();
+
+	require "includes/config.php";
+	require_once "admin/includes/admin.class.php";
+
+	$admin = new Admin();
+
+	if(!$admin->isAdminLoggedIn())
+	{
 		header('Location: index.php');
 		exit();
 	}
-	if(isset($_REQUEST['ar'])){
+
+	if(isset($_REQUEST['ar']))
+	{
 		$stid = $_REQUEST['ar'];
 		$name = $_REQUEST['vn'];
 	}
-?>	
-<?php 
-$pageTitle = "Student Result";
-include "php/headertop_admin.php";
+
+	$pageTitle = "Student Result";
+	include "admin/headertop_admin.php";
 ?>
-<div class="all_student fix">
+
+	
+	<div class="" style="padding-top: 100px;">
 
 		<?php
 			if($_SERVER['REQUEST_METHOD'] == 'POST'){
+
 				$subject = $_POST['subject'];
 				$semester = $_POST['semester'];
 				$marks = $_POST['marks'];
-				$res = $user->add_marks($stid,$subject,$semester,$marks);
-				if($res){
+
+				$res = $admin->addOrUpdateMarks($stid, $subject, $semester, $marks);
+
+				if($res)
+				{
 					echo "<h3 style='color:green;margin:0;padding:0;text-align:center'>Marks successfully inserted!</h3>";
-				}else{
-					echo  "<p style='color:red;text-align:center'>Failed to insert data</p>";
 				}
-			}
-		
-		//SELECT avg(marks) as sgpa from result where st_id=10 and semester="1sr"
+				else
+				{
+					echo "<p style='color:red;text-align:center'>Failed to insert data</p>";
+				}
+			}	
+			//SELECT avg(marks) as sgpa from result where st_id=10 and semester="1sr"
 		?>
-	<div>
-	<p style="text-align:center;color:#fff;background:purple;margin:0;padding:8px;"><?php echo "Name: ".$name."<br>Student ID: " . $stid; ?></p>
-	</div>	
-	<div style="width:40%;margin:50px auto">
-		
-		<table class="tab_one" style="text-align:center;">
-			<form action="" method="post">
-				<table>
+
+		<div class="p-4 mb-4 text-sm text-blue-800 rounded-lg bg-blue-50 dark:bg-gray-800 dark:text-blue-400" style="text-align:center;" role="alert">
+			Name: <?php echo $name; ?><br>
+			Student ID: <?php echo $stid; ?>
+		</div>
+
+		<div style="width:40%;margin:50px auto">
+			<table class="tab_one" style="text-align:center;">
+				<form action="" method="post">
 					<tr>
 						<td>Select Subject: </td>
 						<td>
-						<select name="subject" id="">
-							<option value="DBMS">Database management</option>
-							<option value="DBMS Lab">DBMS Lab</option>
-							<option value="Mathematics">Mathematics</option>
-							<option value="Programming">Programming</option>
-							<option value="Programming Lab">Programming Lab</option>
-							<option value="English">English</option>
-							<option value="Physics">Physics</option>
-							<option value="Chemistry">Chemistry</option>
-							<option value="Psychology">Psychology</option>
-							
-						</select>
+							<select name="subject">
+								<option value="DBMS">Database management</option>
+								<option value="DBMS Lab">DBMS Lab</option>
+								<option value="Mathematics">Mathematics</option>
+								<option value="Programming">Programming</option>
+								<option value="Programming Lab">Programming Lab</option>
+								<option value="English">English</option>
+								<option value="Physics">Physics</option>
+								<option value="Chemistry">Chemistry</option>
+								<option value="Psychology">Psychology</option>
+							</select>
 						</td>
 					</tr>
 					<tr>
 						<td>Select Semester: </td>
 						<td>
-						<select name="semester" id="">
-							<option value="1st">1st semester</option>
-							<option value="2nd">2nd semester</option>
-							<option value="3rd">3rd semester</option>
-						</select>
+							<select name="semester" id="">
+								<option value="1st">1st semester</option>
+								<option value="2nd">2nd semester</option>
+								<option value="3rd">3rd semester</option>
+							</select>
 						</td>
 					</tr>
 					<tr>
@@ -77,16 +87,16 @@ include "php/headertop_admin.php";
 					<tr>
 						<td><input type="submit" name="sub" value="Add marks" /></td>
 						<td><input type="reset" /></td>
-					</tr>
-				</table>
-				
-			</form>
-		</table>
-		
-	</div>
+					</tr>	
+				</form>
+			</table>	
+		</div>
 		<div class="back fix">
-				<p style="text-align:center"><a href="st_result.php"><button class="editbtn">Back to list</button></a></p>
-			</div>
-</div>
-<?php include "php/footerbottom.php";?>
-<?php ob_end_flush() ; ?>
+			<p style="text-align:center"><a href="st_result.php"><button class="editbtn">Back to list</button></a></p>
+		</div>
+	</div>
+
+<?php
+	include "includes/footerbottom.php";
+	ob_end_flush() ; 
+?>
