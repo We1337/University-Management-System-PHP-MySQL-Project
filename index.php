27621667a2c9e8c42cda4e168f189378f60504ca
We1337 <weuser149@gmail.com>
2023-11-04 00:02:42 -0700

    docs: update

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
+"""
+Redirects the user to the "index.html" file located in the "public" directory.
+
+Example Usage:
+    redirect_to_index()
+
+Inputs:
+    None
+
+Outputs:
+    None
+
+"""
+
 <?php
 
 // Redirect to public/inded.html
