69968a3a12227da09fff1349df0894c744aea9e6
We1337 <weuser149@gmail.com>
2023-11-06 17:53:08 -0800

    feater: wellcome page for users

---
+++ public/wellcome.php
@@ -0,0 +1,20 @@
+<?php
+include('includes/header.php');
+?>
+
+<div class="p-4 sm:ml-64" style="padding-top: 6rem;">
+    <div style="padding: 6rem;">
+        <h1 class="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white text-center">We invest in the world’s potential</h1>
+        <p class="mb-6 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400 text-center">Here at Flowbite we focus on markets where technology, innovation, and capital can unlock long-term value and drive economic growth.</p>
+        <a href="#" class="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900 text-center">
+            Learn more
+            <svg class="w-3.5 h-3.5 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
+            </svg>
+        </a>
+    </div>
+</div>
+
+<?php
+include('includes/footer.php');
+?>
