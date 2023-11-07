88a97165722881c51df8bad85e344a817974f16e
We1337 <weuser149@gmail.com>
2023-11-06 17:52:10 -0800

    feater: routing users public pages

--- index.php
+++ index.php
@@ -1,21 +1,5 @@
-"""
-Redirects the user to the "index.html" file located in the "public" directory.
-
-Example Usage:
-    redirect_to_index()
-
-Inputs:
-    None
-
-Outputs:
-    None
-
-"""
-
 <?php
-
-// Redirect to public/inded.html
-header('Location: public/index.html');
-exit;
-
+    // Redirect user to login.php page
+    header('Location: public/login.php');
+    exit();
 ?>
