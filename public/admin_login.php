649be04fffbe90bdb943ccc366f623ef1bedc666
We1337 <weuser149@gmail.com>
2023-11-19 16:56:18 -0800

    refactor: admin page

--- public/admin_login.php
+++ public/admin_login.php
@@ -1,7 +1,23 @@
 <?php
-$title_name = "Admin login";
+ob_start();
+session_start();
+
+$title_name = "Admin authentication";
+$config = include("../private/config/database.php");
+
+require "../private/includes/databaseconnection.php";
+require "../private/admin/admin.class.php";
+
+$admin = new Admin($config);
+
+if($admin->fetchAdminSession()) 
+{
+    header('Location: welcome.php');
+    exit();
+}
 
 include('includes/header.php');
+
 ?>
 
 <div class="p-4 sm:ml-64" style="padding-top: 6rem;">
@@ -17,27 +33,55 @@ include('includes/header.php');
                         <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
                             Sign in to your account
                         </h1>
-                        <form class="space-y-4 md:space-y-6" action="#">
+
+                        <?php
+                        if ($_SERVER['REQUEST_METHOD'] == "POST") 
+                        {
+                            $username = $_POST['username'];
+                            $password = $_POST['password'];
+
+                            if (empty($username) or empty($password)) 
+                            {
+                                echo "<p style='color:red;text-align:center;'>Field must not be empty.</p>";
+                            } 
+                            else 
+                            {
+                                $password = md5($password);
+                                $result = $admin->adminLogin($username, $password);
+
+                                if ($result) 
+                                {
+                                    header('Location: admin/index.php');
+                                } 
+                                else 
+                                {
+                                    echo "<p style='color:red;text-align:center'>Incorrect username or password</p>";
+                                }
+                            }
+                        }
+                        ?>
+
+                        <form class="space-y-4 md:space-y-6" action="#" method="POST">
                             <div>
-                                <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
-                                <input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required="">
+                                <label for="username" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your username or email</label>
+                                <input type="username" name="username" id="username" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required="">
                             </div>
                             <div>
                                 <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
                                 <input type="password" name="password" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="">
                             </div>
                             <div class="flex items-center justify-between">
-                                <div class="flex items-start">
+                                <!-- <div class="flex items-start">
                                     <div class="flex items-center h-5">
                                         <input id="remember" aria-describedby="remember" type="checkbox" class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800" required="">
                                     </div>
                                     <div class="ml-3 text-sm">
                                         <label for="remember" class="text-gray-500 dark:text-gray-300">Remember me</label>
                                     </div>
-                                </div>
+                                </div> -->
                                 <a href="#" class="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500">Forgot password?</a>
                             </div>
-                            <button type="submit" class="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Sign in</button>
+                            <button type="submit" class="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2" valeu="Login">Sign in</button>
                             <p class="text-sm font-light text-gray-500 dark:text-gray-400">
                                 Don’t have an account yet? <a href="#" class="font-medium text-primary-600 hover:underline dark:text-primary-500">Sign up</a>
                             </p>
